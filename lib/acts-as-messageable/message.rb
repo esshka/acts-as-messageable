61365e9c4de1e18680312b71616ae29be6dfc6e5
fahmidur <s.f.reza@gmail.com>
2014-02-04 20:25:55 -0500

    now fixed for marking as read

--- lib/acts-as-messageable/message.rb
+++ lib/acts-as-messageable/message.rb
@@ -8,7 +8,7 @@ module ActsAsMessageable
     belongs_to :sent_messageable,     :polymorphic => true
 
     attr_accessor   :removed, :restored
-    attr_accessible :topic, :body, :recipient_delete, :sender_delete
+    attr_accessible :topic, :body, :recipient_delete, :sender_delete, :opened
     cattr_accessor  :required
 
     ActsAsMessageable.rails_api.new(self).default_scope("created_at desc")
